d51a70a6f9bdec54e78e09d08e6a5e31de202911
HoonJae <rlagnswo0505@naver.com>
2025-03-26 16:20:40 +0900

    feat: 로그인 전 라우트 설정

---
+++ src/app/not-found.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import Link from "next/link";
+import { Card } from "@/components/ui/card";
+
+export default function NotFound() {
+  return (
+    <div
+      className="
+      w-dvw
+      h-dvh
+      flex
+      flex-col
+      justify-center
+      "
+    >
+      <Card
+        className="w-1/2 mx-auto
+        p-4
+        font-bold
+      "
+      >
+        <h1
+          className="
+        text-6xl
+        "
+        >
+          404
+        </h1>
+        <h2
+          className="
+        text-4xl"
+        >
+          Not Found
+        </h2>
+        <h4>이 페이지는 존재하지 않습니다. 다른 페이지를 검색해 보세요.</h4>
+        <Button asChild className="w-fit font-bold">
+          <Link href={"/"}>홈으로</Link>
+        </Button>
+      </Card>
+    </div>
+  );
+}
